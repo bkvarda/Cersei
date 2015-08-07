dc48281f2b9321a9f73ede95a0dcf9d6d143260d
bkvarda <bjkvarda@gmail.com>
2015-08-07 10:31:48 -0700

    got rid of linux

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 //Cersei - the shame client
 
 var express = require('express');
+var fs = require('fs');
+var readLineSync = require('readline-sync');
 var app = express();
 var exec = require('child_process').exec;
 var uri = "https://api.particle.io/v1/events";
-var shame = __dirname + '/sounds/shame.mp3';
-var config = require('./config.json');
-var token = "Bearer " + config.token;
+
+
+
+
+//check for config file, if it doesn't exit prompt for token
+
+if(fs.existsSync('./config.json')){
+  var config = require('./config.json');
+  var token = "Bearer " + config.token;
+}
+else{
+
+  var token = "Bearer " + readLineSync.question("Config file not detected. Please enter your Particle API token:");
+
+}
+
+//set up the SSE client
 var eventSourceInitDict = {rejectUnauthorized: false, headers: {'Authorization': token}};
 var EventSource = require('eventsource');
 
@@ -18,22 +34,20 @@ if(process.platform == 'win32' || 'win64'){
 
 	//it is Windows
 	var player = __dirname + '/players/mpg123-1.22.0-x86-64/mpg123.exe';
+	var shame = 'http://www.myinstants.com/media/sounds/shame-1.mp3';
 }
 if(process.platform == 'darwin'){
 
 	//it is Mac
 	var player = 'afplay';
-}
-else{
-
-	//it is some Linux variant. Assuming this one exists because I doubt there will be many Linux users
-	var player = 'mpg123';
+	var shame = __dirname + '/sounds/shame.mp3';
 }
 
 var es = new EventSource(uri, eventSourceInitDict);
 
 app.listen(1337);
 
+console.log("Cersei is walking with her head down on " + process.platform);
 
 //listens for the shame event on the Particle Cloud and plays the shame clip when it sees the event
 es.addEventListener('shamedingdingding', function(event){
@@ -57,6 +71,8 @@ es.onerror = function(err){
 //Shame! DING DING DING!
 function playShame(callback){
 	console.log("Shame! Ding Ding Ding");
+
+	
 	exec(player +' '+shame, function(error, stdout, stderr){
 		if(error){
 			return callback(error);
